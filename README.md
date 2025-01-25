390cec63daacb7cb663e4b15940fd9d7884f4415
gurasan0110 <gurasan0110@icloud.com>
2025-01-26 07:09:11 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_firebase_practice
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/gurasan0110/flutter_firebase_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gurasan0110.flutter_firebase_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
